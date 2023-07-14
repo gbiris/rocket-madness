9e4ba3d81ea4fc597e517c7891432936877b63e4
Gregory Biris <76085910+gbiris@users.noreply.github.com>
2023-07-14 19:35:39 +1000

    Added particle system
    Added particle system and also fixed up the player controller.

--- RocketMadness/Assets/Scripts/PlayerController.cs
+++ RocketMadness/Assets/Scripts/PlayerController.cs
@@ -11,6 +11,11 @@ public class PlayerController : MonoBehaviour
     private Vector2 startPos;
     private Quaternion startRot;
 
+    public ParticleSystem flames;
+    public ParticleSystem smoke;
+    private ParticleSystem.EmissionModule flamesEmission;
+    private ParticleSystem.EmissionModule smokeEmission;
+
     private float gravity;
     public bool dead;
 
@@ -28,26 +33,42 @@ public class PlayerController : MonoBehaviour
         startRot = base.transform.rotation;
 
         gravity = rb.gravityScale;
+
+        flamesEmission = flames.emission;
+        smokeEmission = smoke.emission; 
+    }
+
+    void Update()
+    {
+        if (Input.GetKey(KeyCode.Space))
+        {
+            flamesEmission.enabled = true;
+        }
+
+        if (Input.GetKeyUp(KeyCode.Space))
+        {
+            flamesEmission.enabled = false;
+        }
     }
 
     void FixedUpdate()
     {
         if (!dead)
+		{
+        Vector2 vel = rb.velocity;
+        float ang = Mathf.Atan2(vel.y, x: 10) * Mathf.Rad2Deg;
+        transform.rotation = Quaternion.Euler(new Vector3(x:0, y:0, z:ang -90));
+
+        if (Input.GetKey(KeyCode.Space))
         {
-            Vector2 vel = rb.velocity;
-            float ang = Mathf.Atan2(vel.y, x: 10) * Mathf.Rad2Deg;
-            transform.rotation = Quaternion.Euler(new Vector3(x:0, y:0, z:ang -90));
+            flamesEmission.enabled = true;
+            rb.AddForce(Vector2.up * 1200f * gravity * Time.deltaTime);
+        }
 
-            if (Input.GetKey(KeyCode.Space))
-            {
-                // flamesEmission.enabled = true;
-                rb.AddForce(Vector2.up * 1200f * gravity * Time.deltaTime);
-            }
-
-            if (Input.GetKeyUp(KeyCode.Space))
-            {
-                // flamesEmission.enabled = false;
-            }
+        if (Input.GetKeyUp(KeyCode.Space))
+        {
+            flamesEmission.enabled = false;
+        }
         }
     }
     #endregion
